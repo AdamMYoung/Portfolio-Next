20b73df30ab7d72ccec8696b15873bff24d4d7eb
Adam Young <adamyoungg97@gmail.com>
2022-08-16 20:01:32 +0100

    next bump

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,33 @@
-import { NextPage } from "next";
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { NextPage } from 'next';
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document: NextPage = () => {
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-WGNHV27GTK"
-          strategy="afterInteractive"
-        />
-        <Script
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732665435466396"
-          strategy="afterInteractive"
-          crossOrigin="anonymous"
-        />
-        <Script
-          id="gtag"
-          dangerouslySetInnerHTML={{
-            __html:
-              " window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WGNHV27GTK');",
-          }}
-          strategy="afterInteractive"
-        />
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html>
+            <Head />
+            <body>
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-WGNHV27GTK" strategy="afterInteractive" />
+                <Script
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732665435466396"
+                    strategy="afterInteractive"
+                    crossOrigin="anonymous"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){window.dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'G-WGNHV27GTK');
+                    `}
+                </Script>
+
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 };
 
 export default Document;
